f1ad40fe35173a0882016be7975d8711ddd121ef
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-07-11 12:40:38 +0500

    logging

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
-
+console.log("🚀 VITE_API_URL:", import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true
