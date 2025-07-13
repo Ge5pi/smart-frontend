1f6548c1fc92a62f3b9e6a410f635036bcfc3504
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-07-13 18:55:36 +0500

    vite

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -14,7 +14,6 @@ const LoginPage = () => {
     const location = useLocation();
     const { login } = useContext(AppContext)!;
 
-    const from = location.state?.from?.pathname || "/";
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
