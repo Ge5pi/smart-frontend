d9c8650dc38f2d124c1f7bb2ab73101323c48a70
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-07-23 18:43:29 +0500

    -апи

--- src/pages/ReportPage.tsx
+++ src/pages/ReportPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import api, { getReport, type EnhancedReport } from '../api';
+import { getReport, type EnhancedReport } from '../api';
 
 const ReportPage: React.FC = () => {
   const { reportId } = useParams<{ reportId: string }>();
