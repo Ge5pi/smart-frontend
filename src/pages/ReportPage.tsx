7eefb99635dee477be7bd56fd59c70eb7f7f5670
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-07-29 15:00:40 +0500

    tuplyu

--- src/pages/ReportPage.tsx
+++ src/pages/ReportPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import type { ReactNode } from 'react';
 import { useParams } from 'react-router-dom';
 import { getReport, type EnhancedReport } from '../api';
