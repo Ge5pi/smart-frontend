c40e86289155f0cd3a142163c4046aece85a5484
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2025-11-22 18:32:38 +0500

    fix

--- src/pages/RequestPasswordResetPage.tsx
+++ src/pages/RequestPasswordResetPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { api } from '../api';
+import api from '../api';
 import { Mail, Loader } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
